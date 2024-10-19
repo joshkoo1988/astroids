43e86d28e97a031529b862c50606e774f4bdb70a
joshkoo1988 <joshkoo1988@gmail.com>
2024-10-19 16:13:25 -0400

    created circleshape.py. added FPS cap at 60 main.py

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from constants import *
 
 def main ():
     ## initalize pygame ##
-    pygame.init
-    
     ## create a screen with constants using pygame.display ##
+    pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    ##create an infinate loop for the game screen ##
+    ## set FPS refresh rate ##
+    clock = pygame.time.Clock()
+    dt = 0
+
+
+    ##create an infinate game loop for the game screen ##
+    ## set fps at 60 ##
     black_color = (0,0,0)
-    while True:
+    running = True
+    while running:
         screen.fill(black_color)
         pygame.display.flip()
+        dt = (clock.tick(60)/1000)
+
+        ## close game if game window is closed ##
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
-    print ("Starting asteroids!")
-    print (f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
-
-    if __name__ == "__main__":
-        main()
 
 main()
