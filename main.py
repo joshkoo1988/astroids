b7e41312e715c4e8a11655e631c376c9ca053451
joshkoo1988 <joshkoo1988@gmail.com>
2024-10-19 15:13:31 -0400

    added a solid screen to main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import *
 
 def main ():
+    ## initalize pygame ##
     pygame.init
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    ## create a screen with constants using pygame.display ##
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    ##create an infinate loop for the game screen ##
+    black_color = (0,0,0)
+    while True:
+        screen.fill(black_color)
+        pygame.display.flip()
+
     print ("Starting asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
