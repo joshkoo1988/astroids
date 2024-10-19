20ce24bf749fb7ce163c16218b1eaf7bd2e5b8e2
joshkoo1988 <joshkoo1988@gmail.com>
2024-10-19 17:01:50 -0400

    draw player added along with player files

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
 def main ():
     ## initalize pygame ##
     ## create a screen with constants using pygame.display ##
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    ## set FPS refresh rate ##
+    ## set FPS refresh rate & create player obj ##
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
 
     ##create an infinate game loop for the game screen ##
     ## set fps at 60 ##
-    black_color = (0,0,0)
     running = True
     while running:
-        screen.fill(black_color)
-        pygame.display.flip()
+        screen.fill((0,0,0))
         dt = (clock.tick(60)/1000)
+
+        ## prints player ##
+        player.draw(screen)
+
+        ## update the display ##
+        pygame.display.flip()
 
         ## close game if game window is closed ##
         for event in pygame.event.get():
